d90c56a69b3108eb0775f532187b4ace39e8db86
RomanUstychenko <roman.ustychenko@gmail.com>
2022-07-29 10:48:26 +0300

    upd timer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,40 +1,24 @@
-import throttle from 'lodash.throttle';
-
 const btnStart = document.querySelector('button[data-start]')
 const btnStop = document.querySelector('button[data-stop]')
 const body = document.querySelector('body')
-console.log(btnStart)
-console.log(btnStop)
-console.log(body)
-const timerId = null;
+let timerId = null;
 
-const onClick = () => {
-   setInterval(() => {
-        body.style.backgroundColor = getRandomHexColor();
-      }, 1000);
-      btnStart.setAttribute('disabled', true)
-    
- 
-  };
+btnStop.setAttribute('disabled', true)
 
-  btnStart.addEventListener('click', throttle(onClick, 1000))
+  btnStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+         body.style.backgroundColor = getRandomHexColor();
+       }, 1000);
+       btnStart.setAttribute('disabled', true)
+       btnStop.removeAttribute('disabled', true)
+   });
+
+   btnStop.addEventListener("click", () => {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled', true)
+  btnStop.setAttribute('disabled', true)
+});
 
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
-
-//   const startBtn = document.querySelector(".js-start");
-// const stopBtn = document.querySelector(".js-stop");
-// let timerId = null;
-
-// startBtn.addEventListener("click", () => {
-//   timerId = setInterval(() => {
-//     console.log(`I love async JS!  ${Math.random()}`);
-//   }, 1000);
-// });
-
-
-// stopBtn.addEventListener("click", () => {
-//   clearInterval(timerId);
-//   console.log(`Interval with id ${timerId} has stopped!`);
-// });
