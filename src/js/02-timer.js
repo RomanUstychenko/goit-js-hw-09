8f348ed7de073ed0fa27c54a6bf7d0091fa13261
RomanUstychenko <roman.ustychenko@gmail.com>
2022-07-29 22:02:55 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,12 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
+const input = document.querySelector('#datetime-picker')
+const btnStart = document.querySelector('button[data-start]')
+
+console.log(input)
+console.log(btnStart)
+
+
+input.addEventListener('click', flatpickr)
+flatpickr("#datetime-picker", {});
