600ff566998f2ea13a2d71c37836ddf4585e93e3
RomanUstychenko <roman.ustychenko@gmail.com>
2022-08-06 20:00:39 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,35 +1,52 @@
-const firstDelay = document.querySelector('input[name=delay]')
-const delayStep = document.querySelector('input[name=step]')
-const amount = document.querySelector('input[name=amount]')
+let delay = document.querySelector('input[name=delay]')
+let delayStep = document.querySelector('input[name=step]')
+const position = document.querySelector('input[name=amount]')
 const form = document.querySelector('form')
 const btn = document.querySelector('button')
-console.log(firstDelay)
-console.log(delayStep)
-console.log(amount)
-console.log(form)
-console.log(btn)
+
 btn.addEventListener("submit", createPromise)
-// const promise = new Promise (() => {})
 
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    console.log('hi')
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+if (shouldResolve) {
+    resolve({position, delay});    }
+ else {      reject({position, delay});
+ }  }, delay);  })
 }
 
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 
 
+// function promiseCreator(counter) {
+//   return new Promise((ok, failed) => {
+//     let total = 0;
 
+//     for (let i = 0; i < counter; i++) {
+//       total += i;
+//     }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//     // console.log(total);
+
+//     if (total % 2 === 0) {
+//       ok(total);
+//     } else {
+//       failed("Error");
+//     }
 //   });
+// }
+// promiseCreator(10)
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+// promiseCreator(1001)
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
