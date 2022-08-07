94ce7817d6c2acaf4bc64daa555aa7659febe1b6
RomanUstychenko <roman.ustychenko@gmail.com>
2022-08-07 15:56:55 +0300

    finish promoses

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,52 +1,39 @@
-let delay = document.querySelector('input[name=delay]')
-let delayStep = document.querySelector('input[name=step]')
-const position = document.querySelector('input[name=amount]')
-const form = document.querySelector('form')
-const btn = document.querySelector('button')
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-btn.addEventListener("submit", createPromise)
-
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-if (shouldResolve) {
-    resolve({position, delay});    }
- else {      reject({position, delay});
- }  }, delay);  })
-}
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-
-
-// function promiseCreator(counter) {
-//   return new Promise((ok, failed) => {
-//     let total = 0;
-
-//     for (let i = 0; i < counter; i++) {
-//       total += i;
-//     }
-
-//     // console.log(total);
-
-//     if (total % 2 === 0) {
-//       ok(total);
-//     } else {
-//       failed("Error");
-//     }
-//   });
-// }
-// promiseCreator(10)
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error));
-
-// promiseCreator(1001)
-//   .then((data) => console.log(data))
-//   .catch((error) => console.log(error));
+  const firstDelay = document.querySelector("input[name=delay]");
+  const delayStep = document.querySelector("input[name=step]");
+  const amountIn = document.querySelector("input[name=amount]");
+  const submitForm = document.querySelector(".form");
+  
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        }
+        else {
+          reject({ position, delay });
+        }
+      }, delay);
+    })
+  }
+  const onButtonClick = (event) => {
+      let delay = Number(firstDelay.value);
+    const step = Number(delayStep.value);
+    const amount = Number(amountIn.value);
+    event.preventDefault();
+    for (let i = 1; i <= amount; i += 1) {
+      createPromise(i, delay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+          Notify.info(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      delay += step;
+    }
+  }
+  submitForm.addEventListener('submit', onButtonClick)
